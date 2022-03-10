fdb014f59b605e4ceb9b681e5ae566f486f91d5a
Reydigital <regysmarie@gmail.com>
2022-03-10 15:46:27 -0800

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
 
  * Description: main - Prints out a string to stdout.
-
- *
 
  * Return: 0 if success.
 
@@ -14,8 +12,8 @@ int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+return (0);
 
 }
