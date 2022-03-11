8ea4796f5727e2bbc8c3079cc33216cb9c99fb0b
Reydigital <regysmarie@gmail.com>
2022-03-10 16:37:02 -0800

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
-
-/*
-
- *Description: main - Prints out a string to stdout.
-
+/**
+Â * main -Entry point
  *
-
- *Return: 0 if success.
-
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-
 printf("with proper grammar, but the outcome is a piece of art,\n");
-
 return(0);
-
 }
