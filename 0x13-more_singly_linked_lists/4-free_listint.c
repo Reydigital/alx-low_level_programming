35cb997ff235ddbec7f37e93ce84eb5627235fd0
Reydigital <regysmarie@gmail.com>
2022-04-25 15:38:03 -0700

    commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,32 @@
+/*
+ * File: 4-free_listint.c
+ * Auth: Régis-Marie
+ */
+
+#include "lists.h"
+ /**
+  * free_listint - Frees a listint_t list.
+  * @head: A pointer to the head of the listint_t list to be freed.
+  */
+
+ void free_listint(listint_t *head)
+
+{
+
+		listint_t *tmp;
+
+
+
+			while (head)
+
+					{
+
+								tmp = head->next;
+
+										free(head);
+
+												head = tmp;
+
+													}
+
+}
