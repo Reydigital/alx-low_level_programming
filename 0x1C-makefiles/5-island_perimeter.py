6e7a0691d7b871d1963406e70934d7091dd4a766
Reydigital <regysmarie@gmail.com>
2022-08-17 18:11:35 -0700

    commit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,77 +1,58 @@
 #!/usr/bin/python3
 
 """
-
     This is the island_perimeter module.
 
     """
 
-
-
-
-
     def neighbour(grid, y, x):
 
             """
+      Finds the number of neighbours of a square
 
-                    Finds the number of neighbours of a square.
+Args:
 
-                            Args:
-
-                                        grid (list): the matrix.
-
-                                                    y (int): height parameter.
-
-                                                                x (int): width parameter.
-
-                                                                    """
-
-                                                                        nb = 4
+grid (list): the matrix.
+y (int): height parameter.
+x (int): width parameter.
 
 
+"""
 
-                                                                            if (y > 0) and grid[y - 1][x]:
+ nb = 4
 
-                                                                                        nb -= 1
+     if (y > 0) and grid[y - 1][x]:
 
-                                                                                            if (x > 0) and grid[y][x - 1]:
+             nb -= 1
 
-                                                                                                        nb -= 1
+                 if (x > 0) and grid[y][x - 1]:
 
-                                                                                                            if (y < len(grid) - 1) and grid[y + 1][x]:
+                         nb -= 1
 
-                                                                                                                        nb -= 1
+                             if (y < len(grid) - 1) and grid[y + 1][x]:                                 nb -= 1
 
-                                                                                                                            if (x < len(grid[0]) - 1) and grid[y][x + 1]:
+     if (x < len(grid[0]) - 1) and grid[y][x + 1]:
 
-                                                                                                                                        nb -= 1
+                                                 nb -= 1
 
-                                                                                                                                            return nb
+                                                     return nb
+      def island_perimeter(grid):
 
+          """
 
+    Finds the island perimeter.                                                                     Args:
 
+                                                                                     grid (list): the matrix.
 
+                                                                                         """
 
-                                                                                                                                        def island_perimeter(grid):
+                                                                                             perimeter = 0
 
-                                                                                                                                                """
+                                                                                                 for y in range(len(grid)):
 
-                                                                                                                                                        Finds the island perimeter.
+                                                                                                         for x in range(len(grid[0])):
 
-                                                                                                                                                                Args:
+                                                                                                                     if grid[y][x] == 1:
 
-                                                                                                                                                                            grid (list): the matrix.
-
-                                                                                                                                                                                """
-
-                                                                                                                                                                                    perimeter = 0
-
-                                                                                                                                                                                        for y in range(len(grid)):
-
-                                                                                                                                                                                                    for x in range(len(grid[0])):
-
-                                                                                                                                                                                                                    if grid[y][x] == 1:
-
-                                                                                                                                                                                                                                        perimeter += neighbour(grid, y, x)
-
-                                                                                                                                                                                                                                            return perimeter
+                                                                                    perimeter += neighbour(grid, y, x)
+                                                                                                                                         return perimeter
